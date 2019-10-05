535a6be623e7bf82d82a341759900622e33b53cf
KG101 <47897150+KG101@users.noreply.github.com>
2019-10-05 12:52:57 -0500

    Make branches adding to fire work.

---
+++ Assets/Scripts/Fireplace.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireplace : InteractableObject
+{
+    [SerializeField]
+    private GameObject player;
+
+    private bool fireBurning = false;
+    private bool playerNearFire=false;
+    private int numberOfSticksInFire;
+    private int sticksInInventory;
+    
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact") &&playerNearFire==true)
+        {
+            AddSticksToFire();
+        }
+    }
+
+    private void AddSticksToFire()
+    {
+        sticksInInventory = player.GetComponent<Inventory>().GetInventory();
+        numberOfSticksInFire += sticksInInventory;
+        Debug.Log(numberOfSticksInFire);
+    }
+
+    public override void Interact()
+    {
+        IntText = "The fire is ";
+        fireBurning = !fireBurning;
+        IntText += fireBurning ? "not burning" : "burning.";
+
+        
+        //GetComponent<Animator>().SetBool("Interact", fireBurning);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log(collision.name);
+            playerNearFire = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerNearFire = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private int numberOfSticks = 0;
+    private GameObject[] inventory = new GameObject[999];
+
+    public void AddItem(GameObject item)
+    {
+        numberOfSticks++;
+    }
+
+    public int GetInventory()
+    {
+        int tempSticks = numberOfSticks;
+        numberOfSticks = 0;
+        return tempSticks;
+    }
+}
