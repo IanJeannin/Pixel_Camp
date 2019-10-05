add0fb7d8b0326b69a4be0ce84a10993431f583e
Ian Jeannin <ian.jeannin@loop.colum.edu>
2019-10-04 23:40:37 -0500

    Add ball toss functionality.
    Closer the player is to ball, the further it should go.  (Slight difference)
    Possible issues: If the player rapidly clicks, they will move the ball again in midair.

---
+++ Assets/Scripts/FetchScripts/Ball.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerCharacter;
+    [SerializeField]
+    private Rigidbody2D ballRigidbody;
+    private bool isInRange = false;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Interact")&&isInRange==true)
+        {
+            TossBall();
+        }
+    }
+
+    private void TossBall()
+    {
+        float playerPos = playerCharacter.transform.position.x;
+        float ballPos = gameObject.transform.position.x;
+        if(playerPos<=ballPos)
+        {
+            ballRigidbody.AddForce(new Vector2(4-(ballPos-playerPos),5),ForceMode2D.Impulse);
+        }
+        else
+        {
+            ballRigidbody.AddForce(new Vector2(-4+(playerPos-ballPos), 5), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            isInRange = true;
+            Debug.Log("Player has entered ball range.");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isInRange = false;
+            Debug.Log("Player has left ball range.");
+        }
+    }
+}
