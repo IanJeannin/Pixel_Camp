83cea0e8bd99ff94e75dd3873bfe606b69278cca
Ian Jeannin <ian.jeannin@loop.colum.edu>
2019-10-25 18:49:11 -0500

    Add a timer to switch times of day

---
+++ Assets/ChangeTime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTime : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject day;
+    [SerializeField]
+    private GameObject night;
+    [SerializeField]
+    private float timeToSwitchTimes = 40;
+
+    private void Start()
+    {
+        StartCoroutine(ChangeToDay());
+    }
+
+    private IEnumerator ChangeToDay()
+    {
+        day.SetActive(true);
+        night.SetActive(false);
+        yield return new WaitForSecondsRealtime(timeToSwitchTimes);
+        StartCoroutine(ChangeToNight());
+    }
+
+    private IEnumerator ChangeToNight()
+    {
+        day.SetActive(false);
+        night.SetActive(true);
+        yield return new WaitForSecondsRealtime(timeToSwitchTimes);
+        StartCoroutine(ChangeToDay());
+    }
+}
